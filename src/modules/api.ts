13f676107a30f6764f0e7d18a71fb5bb6463c5d0
Office11 <jonasbeck@Jonass-MacBook-Pro.local>
2023-11-24 00:35:55 +0100

    api

--- src/modules/api.ts
+++ src/modules/api.ts
@@ -4,47 +4,47 @@ import { store } from "reduxsaga/store"
 import { apiBase, enableSimulate } from "./constants"
 
 interface GetPayload {
-    url: string
-    headers?: any,
-    authorization?: boolean
+  url: string
+  headers?: any,
+  authorization?: boolean
 }
 
 interface PostPayload extends GetPayload {
-    body?: string
+  body?: string
 }
 
 export interface ApiException {
-    code: string | undefined
-    message: string
+  code: string | undefined
+  message: string
 }
 
 const setHeaderAuthrization = (headers: any)=>{
-    const ret = headers?headers:{}
-    const token = store.getState().userReducer.user?.token
-    if (token) {
-        ret['Authorization'] = `Bearer ${token}`
-    }
-    return ret
+  const ret = headers?headers:{}
+  const token = store.getState().userReducer.user?.token
+  if (token) {
+    ret['Authorization'] = `Bearer ${token}`
+  }
+  return ret
 }
 
 const checkStatus = async (response: Response) => {
-    if (response.status != 200) {
-        throw await response.json() as ApiException
-    }
+  if (response.status != 200) {
+    throw await response.json() as ApiException
+  }
 }
 
 const checkError = (json: any) => {
-    const errors = json['error']
-    var exception: ApiException
-    if (errors) {
-        const status = json['status']
-        switch (status) {
-            default:
-                exception = {code: undefined, message: 'Unknown error'}
-                break
-        }
-        throw exception
+  const errors = json['error']
+  var exception: ApiException
+  if (errors) {
+    const status = json['status']
+    switch (status) {
+      default:
+        exception = {code: undefined, message: 'Unknown error'}
+        break
     }
+    throw exception
+  }
 }
 
 export const getAPI = async ({url, headers, authorization}: GetPayload) => {
@@ -89,7 +89,7 @@ async function axiosPost<T>(url: string, data: any, authentication?: boolean) {
         return result.data.data
     } catch (exception: any) {
         if (axios.isAxiosError(exception)) {
-            if (exception.response.data) {
+            if (exception?.response?.data) {
                 throw exception.response.data
             }
         }
@@ -98,77 +98,94 @@ async function axiosPost<T>(url: string, data: any, authentication?: boolean) {
 }
 
 async function axiosGet<T>(url: string, authentication?: boolean) {
-    const token = store.getState().userReducer.user?.token
-    const headers: any = {'Content-Type': 'application/json'}
-    if (authentication != false && token) {
-        headers['Authorization'] = `Bearer ${token}`
+  const token = store.getState().userReducer.user?.token
+  const headers: any = {'Content-Type': 'application/json'}
+  if (authentication != false && token) {
+      headers['Authorization'] = `Bearer ${token}`
+  }
+  console.log('url:', url)
+  try {
+    const result = await axios.get<IApiResponse<T>>(url, {
+      headers
+    })
+    if (result.data.success == false) {
+      throw result.data
     }
-    console.log('url:', url)
-    try {
-        const result = await axios.get<IApiResponse<T>>(url, {
-            headers
-        })
-        if (result.data.success == false) {
-            throw result.data
-        }
-        return result.data.data
-    
-    } catch(error) {
-        if (axios.isAxiosError(exception)) {
-            if (exception.response.data) {
-                throw exception.response.data
-            }
-        }
-        throw exception
+    return result.data.data
+  } catch(exception: any) {
+    if (axios.isAxiosError(exception)) {
+      if (exception?.response?.data) {
+        throw exception.response.data
+      }
     }
+    throw exception
+  }
 }
 
 async function axiosPut<T>(url: string, data?: any, authentication?: boolean) {
     const token = store.getState().userReducer.user?.token
     const headers: any = {'Content-Type': 'application/json'}
     if (authentication != false && token) {
-        headers['Authorization'] = `Bearer ${token}`
+      headers['Authorization'] = `Bearer ${token}`
     }
     console.log('url:', url)
     if (data) {
-        console.log('payload:', data)
+      console.log('payload:', data)
     }
+    try {
     const result = await axios.put<IApiResponse<T>>(url, data, {
-        headers
+      headers
     })
     if (result.data.success == false) {
-        throw result.data
+      throw result.data
     }
     return result.data.data
+  } catch (exception: any) {
+    if (axios.isAxiosError(exception)) {
+      if (exception?.response?.data) {
+        throw exception.response.data
+      }
+    }
+    throw exception
+  }
 }
 
 async function axiosMultiFormPost<T>(url: string, data: FormData, authentication?: boolean) {
-    const token = store.getState().userReducer.user?.token
-    console.log('url:', url)
-    const headers: any = {
-        Accept: '*/*',
-        'Content-Type': 'multipart/form-data'}
-    if (authentication != false && token) {
-        headers['Authorization'] = `Bearer ${token}`
-    }
+  const token = store.getState().userReducer.user?.token
+  console.log('url:', url)
+  const headers: any = {
+    Accept: '*/*',
+    'Content-Type': 'multipart/form-data'}
+  if (authentication != false && token) {
+    headers['Authorization'] = `Bearer ${token}`
+  }
+  try {
     const result = await axios.post<IApiResponse<T>>(url, data, {
-        headers
+      headers
     })
     if (result.data.success == false) {
-        throw result.data
+      throw result.data
     }
     return result.data.data
+  } catch (exception: any) {
+    if (axios.isAxiosError(exception)) {
+      if (exception?.response?.data) {
+        throw exception.response.data
+      }
+    }
+    throw exception
+  }
 }
 
 export interface ILoginPayload {
-    email: string
-    password: string
+  email: string
+  password: string
 }
 
 export function apiLogin({}: ILoginPayload) {
-    if (enableSimulate) {
-    } else {
-        throw new Error("not implemented.")
-        
-    }    
+  if (enableSimulate) {
+  } else {
+    throw new Error("not implemented.")
+      
+  }    
 }
